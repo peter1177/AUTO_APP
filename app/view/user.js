d26fea28fb98d9815777bc8f4e5711f2459fe969
liandti <duan@echoman.net>
2016-08-07 19:46:45 +0800

    新建用户信息页

---
+++ app/view/user.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Navigator
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  topview: {
+    flex: 1,
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    //justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5ab0e6',
+    height: 135
+  }
+  return:{
+
+  }
+})
+
+export default class User {
+  constructor() {
+
+  }
+  render(){
+    return(
+      <View style={styles.topview}>
+        <View style={styles.less}>
+                  <
+        </View>
+      </View>
+    )
+  }
+}
